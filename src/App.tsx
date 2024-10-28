b3fc7706677b0280a91403c24b060b7deb0e869b
Ryan Feigenbaum <ryan.feigenbaum@gmail.com>
2024-10-28 09:52:15 -0400

    feat: Add Vite environment declaration file
    Add vite-env.d.ts file to reference types for Vite client.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { FreakyShiki } from "./FreakyShiki";
+
+function App() {
+  return (
+    <div className="container p-24 mx-auto">
+      <h1 className="text-xl font-bold">Freaky Shiki</h1>
+      <FreakyShiki />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/FreakyShiki.tsx
@@ -0,0 +1,63 @@
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import { format } from "prettier";
+import babel from "prettier/plugins/babel";
+import estree from "prettier/plugins/estree";
+import postcss from "prettier/plugins/postcss";
+import typescript from "prettier/plugins/typescript";
+import { useState } from "react";
+import { codeToHtml } from "shiki";
+
+export function FreakyShiki() {
+  const [html, setHtml] = useState<string | null>(null);
+
+  const [lang, setLang] = useState<string>("ts");
+
+  async function handleChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
+    try {
+      const formattedCode = await format(event.target.value, {
+        parser: "typescript",
+        plugins: [postcss, babel, estree, typescript],
+      });
+
+      const html = await codeToHtml(formattedCode, {
+        lang: "typescript",
+        themes: {
+          light: "min-light",
+          dark: "dark-plus",
+        },
+      });
+
+      const wrappedHtml = codeWrapper({ lang, html });
+
+      setHtml(wrappedHtml);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  function copy() {
+    navigator.clipboard.writeText(html || "");
+  }
+
+  return (
+    <div>
+      <Textarea className="mb-2" onChange={handleChange} />
+      <Button onClick={copy}>Copy</Button>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: html || "",
+        }}
+      ></div>
+      <div>{html}</div>
+    </div>
+  );
+}
+
+type CodeWrapperType = {
+  lang: string;
+  html: string;
+};
+
+const codeWrapper = ({ lang, html }: CodeWrapperType) =>
+  `<div class="s-code"><div class="s-code-nav"><span>${lang}</span><button aria-label="Copy code" class="s-code-copy"><svg viewBox="0 0 24 24" fill="currentColor"><path d="M6.9998 6V3C6.9998 2.44772 7.44752 2 7.9998 2H19.9998C20.5521 2 20.9998 2.44772 20.9998 3V17C20.9998 17.5523 20.5521 18 19.9998 18H16.9998V20.9991C16.9998 21.5519 16.5499 22 15.993 22H4.00666C3.45059 22 3 21.5554 3 20.9991L3.0026 7.00087C3.0027 6.44811 3.45264 6 4.00942 6H6.9998ZM5.00242 8L5.00019 20H14.9998V8H5.00242ZM8.9998 6H16.9998V16H18.9998V4H8.9998V6ZM7 11H13V13H7V11ZM7 15H13V17H7V15Z"></path></svg></button></div>${html}</div>`;
