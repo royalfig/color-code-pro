a5c33326a944310e886aea441167a0649ef2643e
Ryan Feigenbaum <ryan.feigenbaum@gmail.com>
2024-11-03 14:48:37 -0500

    yep

--- src/FreakyShiki.tsx
+++ src/FreakyShiki.tsx
@@ -7,7 +7,7 @@ import postcss from "prettier/plugins/postcss";
 import typescript from "prettier/plugins/typescript";
 import html from "prettier/plugins/html";
 import yaml from "prettier/plugins/yaml";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { codeToHtml } from "shiki";
 import {
   Select,
@@ -23,46 +23,66 @@ const langMap = new Map([
   ["css", "css"],
   ["json", "json"],
   ["html", "html"],
-  ["yaml", "yaml"]
-]
-)
+  ["yaml", "yaml"],
+]);
 
 console.log(langMap.get("typescript"));
 
 export function FreakyShiki() {
   const [renderedHtml, updateRenderedHtml] = useState<string | null>(null);
   const [lang, setLang] = useState<string>("typescript");
+  const text = useRef<HTMLTextAreaElement>(null);
 
   async function handleChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
     try {
-      const formattedCode = await format(event.target.value, {
-        parser: langMap.get(lang),
-        plugins: [postcss, babel, estree, typescript, html, yaml],
-      });
-
-      const rawHtml = await codeToHtml(formattedCode, {
+      const rawHtml = await codeToHtml(event.target.value, {
         lang: lang,
         themes: {
           light: "min-light",
-          dark: 'dark-plus',
+          dark: "dark-plus",
         },
         colorReplacements: {
-          'dark-plus': {
-            '#1e1e1e': 'var(--cl-surface)'
-          }
-        }
+          "dark-plus": {
+            "#1e1e1e": "var(--cl-surface)",
+          },
+        },
       });
 
-      const wrappedHtml = codeWrapper({ lang, rawHtml });
-
-      updateRenderedHtml(wrappedHtml);
+      updateRenderedHtml(rawHtml);
     } catch (error) {
       console.error(error);
     }
   }
 
+  async function formatCode() {
+    if (!text.current) return;
+
+    const formatted = await format(text.current.value, {
+      parser: langMap.get(lang),
+      plugins: [postcss, babel, estree, typescript, html, yaml],
+    });
+
+    const stylized = await codeToHtml(formatted, {
+      lang: lang,
+      themes: {
+        light: "min-light",
+        dark: "dark-plus",
+      },
+      colorReplacements: {
+        "dark-plus": {
+          "#1e1e1e": "var(--cl-surface)",
+        },
+      },
+    });
+
+    updateRenderedHtml(stylized);
+  }
+
   function copy() {
-    navigator.clipboard.writeText(renderedHtml || "");
+    if (!renderedHtml) return;
+    const wrappedHtml = codeWrapper({ lang, renderedHtml });
+
+    navigator.clipboard.writeText(wrappedHtml || "");
   }
 
   return (
@@ -80,23 +100,31 @@ export function FreakyShiki() {
           <SelectItem value="yaml">YAML</SelectItem>
         </SelectContent>
       </Select>
-      <Textarea onChange={handleChange} className="min-h-80" />
-      <Button onClick={copy}>Copy</Button>
-      <div
-        dangerouslySetInnerHTML={{
-          __html: renderedHtml || "",
-        }}
-      ></div>
-      <div>{renderedHtml}</div>
+      <div className="relative border rounded-md p-2 min-h-80 font-mono leading-tight">
+        <div
+          className="relative  w-full h-full "
+          dangerouslySetInnerHTML={{
+            __html: renderedHtml || "",
+          }}
+        ></div>
+        <textarea
+          onChange={handleChange}
+          className=" w-full h-full z-10 absolute inset-0 p-2 text-transparent caret-red-600 bg-transparent"
+          ref={text}
+        />
+      </div>
+      <div className="flex gap-2">
+        <Button onClick={copy}>Copy</Button>
+        <Button onClick={formatCode}>Format</Button>
+      </div>
     </div>
   );
 }
 
 type CodeWrapperType = {
   lang: string;
-  rawHtml: string;
+  renderedHtml: string;
 };
 
-const codeWrapper = ({ lang, rawHtml }: CodeWrapperType) =>
-  `<div class="s-code"><div class="s-code-nav"><span>${lang}</span><button aria-label="Copy code" class="s-code-copy"><svg viewBox="0 0 24 24" fill="currentColor"><path d="M6.9998 6V3C6.9998 2.44772 7.44752 2 7.9998 2H19.9998C20.5521 2 20.9998 2.44772 20.9998 3V17C20.9998 17.5523 20.5521 18 19.9998 18H16.9998V20.9991C16.9998 21.5519 16.5499 22 15.993 22H4.00666C3.45059 22 3 21.5554 3 20.9991L3.0026 7.00087C3.0027 6.44811 3.45264 6 4.00942 6H6.9998ZM5.00242 8L5.00019 20H14.9998V8H5.00242ZM8.9998 6H16.9998V16H18.9998V4H8.9998V6ZM7 11H13V13H7V11ZM7 15H13V17H7V15Z"></path></svg></button></div>${rawHtml}</div>`;
-
+const codeWrapper = ({ lang, renderedHtml }: CodeWrapperType) =>
+  `<div class="s-code"><div class="s-code-nav"><span>${lang}</span><button aria-label="Copy code" class="s-code-copy"><svg viewBox="0 0 24 24" fill="currentColor"><path d="M6.9998 6V3C6.9998 2.44772 7.44752 2 7.9998 2H19.9998C20.5521 2 20.9998 2.44772 20.9998 3V17C20.9998 17.5523 20.5521 18 19.9998 18H16.9998V20.9991C16.9998 21.5519 16.5499 22 15.993 22H4.00666C3.45059 22 3 21.5554 3 20.9991L3.0026 7.00087C3.0027 6.44811 3.45264 6 4.00942 6H6.9998ZM5.00242 8L5.00019 20H14.9998V8H5.00242ZM8.9998 6H16.9998V16H18.9998V4H8.9998V6ZM7 11H13V13H7V11ZM7 15H13V17H7V15Z"></path></svg></button></div>${renderedHtml}</div>`;
